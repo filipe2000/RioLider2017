c784aa45b3fdf7a736cc90dea45d68b74e2586e0
Filipe Souza <filipeosouza2000@hotmail.com>
2017-05-30 11:26:54 -0300

    Produtos

---
+++ includes/produtos/init.php
@@ -0,0 +1,9 @@
+				
+<div id="divProd" align="center">
+	<h5>Acesse nosso catálogo para visualizar todos os produtos.</h5>
+	<br>
+	<div class="subProd caixas">
+		<span class="loading"></span>
+	</div>
+	
+</div>
